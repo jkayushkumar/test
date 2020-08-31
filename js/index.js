76ab249f4477c97a809f15d58bbd7afc93d51957
Ayush <ayush.kumar20@yahoo.in>
2020-08-31 15:46:21 +0530

    test

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ function diveIN()
     if(depth1)
     {
         document.getElementById("sea").classList.add("droplets2");
+        document.getElementsByTagName('meta')["theme-color"].content = "#0568A6";
         document.getElementById("sea").classList.remove("droplets1");  
         document.getElementById("sea").classList.remove("droplets3"); 
         document.getElementById("diveOut").classList.remove("noDisplay");     
@@ -15,6 +16,7 @@ function diveIN()
         if(depth2)
         {
             document.getElementById("sea").classList.add("droplets3");
+            document.getElementsByTagName('meta')["theme-color"].content = "#022340";
             document.getElementById("sea").classList.remove("droplets1");  
             document.getElementById("sea").classList.remove("droplets2"); 
             document.getElementById("diveIn").classList.add("noDisplay"); 
@@ -36,6 +38,7 @@ function diveOUT()
         {
             document.getElementById("diveOut").classList.add("noDisplay");
             document.getElementById("sea").classList.add("droplets1");  
+            document.getElementsByTagName('meta')["theme-color"].content = "#11a8e4";
             document.getElementById("sea").classList.remove("droplets2");
             document.getElementById("sea").classList.remove("droplets3");
         }   
@@ -43,7 +46,8 @@ function diveOUT()
         {
             if(depth3)
             {
-                document.getElementById("sea").classList.add("droplets2");  
+                document.getElementById("sea").classList.add("droplets2"); 
+                document.getElementsByTagName('meta')["theme-color"].content = "#0568A6"; 
                 document.getElementById("sea").classList.remove("droplets1");
                 document.getElementById("sea").classList.remove("droplets3");
                 document.getElementById("diveIn").classList.remove("noDisplay");
