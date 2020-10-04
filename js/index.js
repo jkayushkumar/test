d489eeeb403d835ed1fdc047669c10e60f9ef9c5
Ayush <ayush.kumar20@yahoo.in>
2020-10-04 20:33:19 +0530

    bug fix

--- js/index.js
+++ js/index.js
@@ -4,108 +4,123 @@ window.onload = function () {
         document.getElementById("depth3").classList.add("noDisplay");
     },100);
 }
+var updatingCSS =false;
 function diveIN()
 {  
-    document.getElementById("diveIn").classList.add("noClick");
+    if(!updatingCSS)
+    {
+        document.getElementById("diveIn").classList.add("noClick");
+        updatingCSS=true;
 
-    var depth1=(document.getElementById("depth1").classList.contains("ontop"));
-    var depth2=(document.getElementById("depth2").classList.contains("ontop"));
-    var depth3=(document.getElementById("depth3").classList.contains("ontop"));
-    if(!depth1 && !depth2 && !depth3)
-    {   
-        document.getElementById("depth1").classList.add("ontop");
-        document.getElementById("depth2").classList.remove("ontop");
-        document.getElementById("depth3").classList.remove("ontop");
-        document.getElementById("diveOut").classList.remove("noDisplay");
-        $('html, body').animate({scrollTop: $("#depth1").offset().top}, 1500);
-        window.setTimeout(function (){
-            document.getElementsByTagName('meta')["theme-color"].content = "#0da3d5"; 
-            document.getElementById("surface").classList.add("noDisplay");
-            document.getElementById("diveIn").classList.remove("noClick");
-        },1500);
-       
+        var depth1=(document.getElementById("depth1").classList.contains("ontop"));
+        var depth2=(document.getElementById("depth2").classList.contains("ontop"));
+        var depth3=(document.getElementById("depth3").classList.contains("ontop"));
+        if(!depth1 && !depth2 && !depth3)
+        {   
+            document.getElementById("depth1").classList.add("ontop");
+            document.getElementById("depth2").classList.remove("ontop");
+            document.getElementById("depth3").classList.remove("ontop");
+            document.getElementById("diveOut").classList.remove("noDisplay");
+            $('html, body').animate({scrollTop: $("#depth1").offset().top}, 1500);
+            window.setTimeout(function (){
+                document.getElementsByTagName('meta')["theme-color"].content = "#0da3d5"; 
+                document.getElementById("surface").classList.add("noDisplay");
+                document.getElementById("diveIn").classList.remove("noClick");
+                updatingCSS=false;
+            },1500);
+            
+        }
+        if(depth1 && !depth2 && !depth3)
+        {
+            document.getElementById("depth2").classList.remove("noDisplay");        
+            document.getElementById("depth2").classList.add("ontop");        
+            document.getElementById("depth1").classList.remove("ontop");
+            document.getElementById("depth3").classList.remove("ontop");
+            $('html, body').animate({scrollTop: $("#depth2").offset().top}, 1500);
+            window.setTimeout(function (){
+                document.getElementsByTagName('meta')["theme-color"].content = "#0568A6";
+                document.getElementById("depth1").classList.add("noDisplay"); 
+                document.getElementById("diveIn").classList.remove("noClick");
+                updatingCSS=false;           
+            },1500);
+        }
+        if(!depth1 && depth2 && !depth3)
+        {
+            document.getElementById("depth3").classList.remove("noDisplay");
+            document.getElementById("depth3").classList.add("ontop");
+            document.getElementById("depth1").classList.remove("ontop");
+            document.getElementById("depth2").classList.remove("ontop");
+            document.getElementById("diveIn").classList.add("noDisplay");
+            $('html, body').animate({scrollTop: $("#depth3").offset().top}, 1500);
+            window.setTimeout(function (){
+                document.getElementsByTagName('meta')["theme-color"].content = "#022340";
+                document.getElementById("depth2").classList.add("noDisplay"); 
+                document.getElementById("diveIn").classList.remove("noClick");
+                updatingCSS=false;
+            },1500);
+        }
     }
-    if(depth1 && !depth2 && !depth3)
-    {
-        document.getElementById("depth2").classList.remove("noDisplay");        
-        document.getElementById("depth2").classList.add("ontop");        
-        document.getElementById("depth1").classList.remove("ontop");
-        document.getElementById("depth3").classList.remove("ontop");
-        $('html, body').animate({scrollTop: $("#depth2").offset().top}, 1500);
-        window.setTimeout(function (){
-            document.getElementsByTagName('meta')["theme-color"].content = "#0568A6";
-            document.getElementById("depth1").classList.add("noDisplay"); 
-            document.getElementById("diveIn").classList.remove("noClick");           
-        },1500);
-    }
-    if(!depth1 && depth2 && !depth3)
-    {
-        document.getElementById("depth3").classList.remove("noDisplay");
-        document.getElementById("depth3").classList.add("ontop");
-        document.getElementById("depth1").classList.remove("ontop");
-        document.getElementById("depth2").classList.remove("ontop");
-        document.getElementById("diveIn").classList.add("noDisplay");
-        $('html, body').animate({scrollTop: $("#depth3").offset().top}, 1500);
-        window.setTimeout(function (){
-            document.getElementsByTagName('meta')["theme-color"].content = "#022340";
-            document.getElementById("depth2").classList.add("noDisplay"); 
-            document.getElementById("diveIn").classList.remove("noClick");
-        },1500);
-    }
-   
+    
 }
-    function diveOUT()
+function diveOUT()
 {
-    document.getElementById("diveOut").classList.add("noClick");
+    if(!updatingCSS)
+    {
+        document.getElementById("diveOut").classList.add("noClick");
+        updatingCSS=true;
 
-    var depth1=(document.getElementById("depth1").classList.contains("ontop"));
-    var depth2=(document.getElementById("depth2").classList.contains("ontop"));
-    var depth3=(document.getElementById("depth3").classList.contains("ontop"));
-    if(!depth1 && !depth2 && !depth3)
-    {
-    }
-    if(depth1 && !depth2 && !depth3)
-    {
-        document.getElementById("surface").classList.remove("noDisplay");
-        document.getElementById("depth1").classList.remove("ontop");
-        document.getElementById("depth2").classList.remove("ontop");
-        document.getElementById("depth3").classList.remove("ontop");
-        document.getElementById("diveOut").classList.add("noDisplay");
-        document.getElementById("diveIn").classList.remove("noDisplay");
-        $('html, body').animate({scrollTop: $("#surface").offset().top}, 1500);
-        window.setTimeout(function (){
-            document.getElementsByTagName('meta')["theme-color"].content = "#1F3B73";
-            document.getElementById("diveOut").classList.remove("noClick");
-        },1500);
+        var depth1=(document.getElementById("depth1").classList.contains("ontop"));
+        var depth2=(document.getElementById("depth2").classList.contains("ontop"));
+        var depth3=(document.getElementById("depth3").classList.contains("ontop"));
+        if(!depth1 && !depth2 && !depth3)
+        {
+        }
+        if(depth1 && !depth2 && !depth3)
+        {
+            document.getElementById("surface").classList.remove("noDisplay");
+            document.getElementById("depth1").classList.remove("ontop");
+            document.getElementById("depth2").classList.remove("ontop");
+            document.getElementById("depth3").classList.remove("ontop");
+            document.getElementById("diveOut").classList.add("noDisplay");
+            document.getElementById("diveIn").classList.remove("noDisplay");
+            $('html, body').animate({scrollTop: $("#surface").offset().top}, 1500);
+            window.setTimeout(function (){
+                document.getElementsByTagName('meta')["theme-color"].content = "#1F3B73";
+                document.getElementById("diveOut").classList.remove("noClick");
+                updatingCSS=false;
+            },1500);
         
-    }
-    if(!depth1 && depth2 && !depth3)
-    {
-        document.getElementById("depth1").classList.remove("noDisplay");
-        document.getElementById("depth1").classList.add("ontop");
-        document.getElementById("depth2").classList.remove("ontop");
-        document.getElementById("depth3").classList.remove("ontop");
-        $('html, body').animate({scrollTop: $("#depth1").offset().top}, 1500);
-        window.setTimeout(function (){
-            document.getElementsByTagName('meta')["theme-color"].content = "#0da3d5"; 
-            document.getElementById("depth2").classList.add("noDisplay");
-            document.getElementById("depth3").classList.add("noDisplay");
-            document.getElementById("diveOut").classList.remove("noClick");
-        },1500);
-    }
-    if(!depth1 && !depth2 && depth3)
-    {
-        document.getElementById("depth2").classList.remove("noDisplay")
-        document.getElementById("depth2").classList.add("ontop");
-        document.getElementById("depth3").classList.remove("ontop");
-        document.getElementById("depth1").classList.remove("ontop");
-        document.getElementById("diveIn").classList.remove("noDisplay");
-        $('html, body').animate({scrollTop: $("#depth2").offset().top}, 1500);
-        window.setTimeout(function (){
-            document.getElementsByTagName('meta')["theme-color"].content = "#0568A6"; 
-            document.getElementById("depth1").classList.add("noDisplay");
-            document.getElementById("depth3").classList.add("noDisplay");
-            document.getElementById("diveOut").classList.remove("noClick");
-        },1500);
+        }
+        if(!depth1 && depth2 && !depth3)
+        {
+            document.getElementById("depth1").classList.remove("noDisplay");
+            document.getElementById("depth1").classList.add("ontop");
+            document.getElementById("depth2").classList.remove("ontop");
+            document.getElementById("depth3").classList.remove("ontop");
+            $('html, body').animate({scrollTop: $("#depth1").offset().top}, 1500);
+            window.setTimeout(function (){
+                document.getElementsByTagName('meta')["theme-color"].content = "#0da3d5"; 
+                document.getElementById("depth2").classList.add("noDisplay");
+                document.getElementById("depth3").classList.add("noDisplay");
+                document.getElementById("diveOut").classList.remove("noClick");
+                updatingCSS=false;
+            },1500);
+        }
+        if(!depth1 && !depth2 && depth3)
+        {
+            document.getElementById("depth2").classList.remove("noDisplay")
+            document.getElementById("depth2").classList.add("ontop");
+            document.getElementById("depth3").classList.remove("ontop");
+            document.getElementById("depth1").classList.remove("ontop");
+            document.getElementById("diveIn").classList.remove("noDisplay");
+            $('html, body').animate({scrollTop: $("#depth2").offset().top}, 1500);
+            window.setTimeout(function (){
+                document.getElementsByTagName('meta')["theme-color"].content = "#0568A6"; 
+                document.getElementById("depth1").classList.add("noDisplay");
+                document.getElementById("depth3").classList.add("noDisplay");
+                document.getElementById("diveOut").classList.remove("noClick");
+                updatingCSS=false;
+            },1500);
+        }   
     }
 }
