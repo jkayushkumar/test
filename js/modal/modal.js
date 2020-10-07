9264c7b83ad4277694891544669fda718025520e
Ayush <ayush.kumar20@yahoo.in>
2020-10-08 02:02:56 +0530

    Deploy

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -5,12 +5,13 @@ function showContactModal() {
   document.getElementById("contactModal").style.display = "grid";
 }
 
-function contactClose() {
-  document.getElementById("contactModal").style.display = "none";
+function showResumeModal() {
+  document.getElementById("resumeModal").style.display = "grid";
 }
 
 window.onclick = function(event) {
-  if (event.target == document.getElementById("contactModal")) {
+  if (event.target == document.getElementById("contactModal")||event.target == document.getElementById("resumeModal")) {
     document.getElementById("contactModal").style.display = "none";
+    document.getElementById("resumeModal").style.display = "none";
   }
 }
